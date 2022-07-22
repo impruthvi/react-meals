d056c42beb0bbce41358b961a8b20d2bc599924a
Pruthvisinh Rajput <pruthvisinh.rajput@yudiz.com>
2022-07-22 11:53:07 +0530

    manage the states for modal show and hide

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,6 @@
 import classes from "./Cart.module.css";
 import Model from "../UI/Modal";
-const Cart = () => {
+const Cart = (props) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "Sushi", amount: "1", price: 100 }].map((item) => {
@@ -8,15 +8,18 @@ const Cart = () => {
       })}
     </ul>
   );
+
   return (
-    <Model>
+    <Model onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>105</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
     </Model>
