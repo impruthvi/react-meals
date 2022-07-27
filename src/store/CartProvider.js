5cc5ec6e544b156510e9068385d6b1378195ee93
Pruthvisinh Rajput <pruthvisinh.rajput@yudiz.com>
2022-07-27 12:24:08 +0530

    making cart item removable

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -33,6 +33,31 @@ const cartReducer = (state, action) => {
       totalAmount: updateTotalAmount,
     };
   }
+
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const exitingCartItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - exitingCartItem.price;
+    let updatedItems;
+    if (exitingCartItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = {
+        ...exitingCartItem,
+        amount: exitingCartItem.amount - 1,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
   return defaultCartState;
 };
 
