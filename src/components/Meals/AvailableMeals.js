6d9f9edf872e5e78bd0e02b871293c5d46a522f3
Pruthvisinh Rajput <pruthvisinh.rajput@yudiz.com>
2022-08-26 14:27:09 +0530

    added a error handling

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,38 +1,56 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
-
-  const [meals,setMeals] =useState([]);
-  const [isLoading,setIsLoading] =useState(true);
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
-      const response = await fetch("https://react-http-852b3-default-rtdb.firebaseio.com/meals.json");
+      const response = await fetch(
+        "https://react-http-852b3-default-rtdb.firebaseio.com/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
       const responseData = await response.json();
 
       const loadedMeals = [];
-      for(const key in responseData) {
+      for (const key in responseData) {
         loadedMeals.push({
           id: key,
-          name:responseData[key].name,
-          description:responseData[key].description,
-          price:responseData[key].price,
-
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
         });
       }
       setMeals(loadedMeals);
       setIsLoading(false);
     };
-    fetchMeals();
+
+      fetchMeals().catch(error =>{
+        setIsLoading(false);
+        setHttpError(error.message);
+      });
+    
   }, []);
 
-  if(isLoading){
+  if (isLoading) {
     return (
       <section className={classes.MealIsLoading}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
